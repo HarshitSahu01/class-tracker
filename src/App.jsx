42a303beb081d896286e9ed3e0d5976f08496429
jnt48 <162683634+jnt48@users.noreply.github.com>
2024-10-18 10:57:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import Home from './Pages/Home'
-import Navbar from './Components/Navbar'
+import RegisterPage from './Pages/StudentRegistration'
+import StudentLogin from './Pages/StudentLogin'
+import ManagmentRegister from './Pages/ManagmentRegister'
+import ManagmentLogin from './Pages/ManagmentLogin'
+import Home1 from './Pages/Home1'
+import BuildingForm from './Pages/Form'
+import FormClassRoom from './Pages/FormClassRoom'
 
 function App() {
   return (
     <>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Home1" element={<Home1 />} />
+        <Route path="/StudentRegistration" element={<RegisterPage />} />
+        <Route path="/StudentLogin" element={<StudentLogin />} />
+        <Route path="/ManagmentRegistration" element={<ManagmentRegister />} />
+        <Route path="/ManagmentLogin" element={<ManagmentLogin />} />
+        <Route path="/formb" element={<BuildingForm />} />
+        <Route path="/formc" element={<FormClassRoom />} />
       </Routes>
+
     </>
   )
 }
